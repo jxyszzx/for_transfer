df88dcb3e5495c895a15670ce8dc1dd8ff5c2c23
jxyszzx <jxyszzx@163.com>
2020-09-15 19:25:49 +0800

    sync log

--- codes/n-hop/for_github_issue.cpp
+++ codes/n-hop/for_github_issue.cpp
@@ -1,6 +1,7 @@
 #include <cstdint>
 #include <cstdlib>
 #include <utility>
+#include <unordered_set>
 
 #include "plato/util/perf.hpp"
 #include "plato/util/atomic.hpp"
@@ -15,131 +16,137 @@ DEFINE_uint32(n,           5,      "at most N-hop");
 DEFINE_uint32(k,           4,      "len of cycle >= k");
 DEFINE_uint32(num,         1,      "num of cycle");
 DEFINE_string(subnode,     "",     "subnode");
+DEFINE_uint32(sub_round,   10,     "nodes number for one round");
 
 void init(int argc, char** argv) {
-    gflags::ParseCommandLineFlags(&argc, &argv, true);
-    google::InitGoogleLogging(argv[0]);
-    google::LogToStderr();
+  gflags::ParseCommandLineFlags(&argc, &argv, true);
+  google::InitGoogleLogging(argv[0]);
+  google::LogToStderr();
 }
 
 int main(int argc, char** argv) {
-    auto& cluster_info = plato::cluster_info_t::get_instance();
+  auto& cluster_info = plato::cluster_info_t::get_instance();
 
-    init(argc, argv);
-    cluster_info.initialize(&argc, &argv);
+  init(argc, argv);
+  cluster_info.initialize(&argc, &argv);
 
-    // init graph
-    plato::graph_info_t graph_info(FLAGS_is_directed);
-    auto pdcsc = plato::create_dcsc_seqs_from_path<plato::empty_t>(&graph_info, FLAGS_input,
-        plato::edge_format_t::CSV, plato::dummy_decoder<plato::empty_t>,
-        FLAGS_alpha, FLAGS_part_by_in);
+  // init graph
+  plato::graph_info_t graph_info(FLAGS_is_directed);
+  auto pdcsc = plato::create_dcsc_seqs_from_path<plato::empty_t>(&graph_info, FLAGS_input,
+      plato::edge_format_t::CSV, plato::dummy_decoder<plato::empty_t>,
+      FLAGS_alpha, FLAGS_part_by_in);
 
-    using graph_spec_t         = std::remove_reference<decltype(*pdcsc)>::type;
-    using partition_t          = graph_spec_t::partition_t;
-    using adj_unit_list_spec_t = graph_spec_t::adj_unit_list_spec_t;
-    using path_state_t = plato::dense_state_t<std::vector<std::vector<int>>, partition_t>;
-    using bitmap_spec_t        = plato::bitmap_t<>;
+  using graph_spec_t         = std::remove_reference<decltype(*pdcsc)>::type;
+  using partition_t          = graph_spec_t::partition_t;
+  using adj_unit_list_spec_t = graph_spec_t::adj_unit_list_spec_t;
+  using path_state_t         = plato::dense_state_t<std::vector<std::vector<int>>, partition_t>;
+  using bitmap_spec_t        = plato::bitmap_t<>;
+  using context_spec_t       = plato::mepa_ag_context_t<std::vector<std::vector<int>>>;
+  using message_spec_t       = plato::mepa_ag_message_t<std::vector<std::vector<int>>>;
 
-    auto out_degrees = plato::generate_dense_out_degrees_fg<uint32_t>(graph_info, *pdcsc, false);;
+  plato::bsp_opts_t opts;
+  opts.local_capacity_ = PAGESIZE/16;
+  opts.global_size_ = 16*MBYTES;
+  
+  std::vector<std::mutex> mtx_next(graph_info.vertices_), mtx_found(graph_info.vertices_);
 
-    // read subnode
+  std::ifstream fin{FLAGS_subnode};
+  for (int round_i = 1; round_i <= 10; ++round_i) {
+    std::shared_ptr<path_state_t> curr_path(new path_state_t(graph_info.vertices_, pdcsc->partitioner()));
+    std::shared_ptr<path_state_t> next_path(new path_state_t(graph_info.vertices_, pdcsc->partitioner()));
+    std::shared_ptr<path_state_t> found_path(new path_state_t(graph_info.vertices_, pdcsc->partitioner()));
     std::shared_ptr<bitmap_spec_t> existed(new bitmap_spec_t(graph_info.vertices_));
+    // read subnode
     existed->clear();
     {
-        std::ifstream fin{FLAGS_subnode};
-        int u;
-        while (fin >> u) {
-            existed->set_bit(u);
-        }
+      int u;
+      for (int i = 0; i < std::min((uint32_t)20, FLAGS_sub_round); ++i) {
+        fin >> u;
+        existed->set_bit(u);
+      }
+      for (int i = FLAGS_sub_round; i < 20; ++i) {
+        fin >> u;
+      }
     }
-
-    // plato::bsp_opts_t opts;
-    // opts.local_capacity_ = PAGESIZE;
-    // opts.global_size_ = MBYTES;
-
-    std::shared_ptr<path_state_t> curr_path(new path_state_t(graph_info.max_v_i_, pdcsc->partitioner()));
-    std::shared_ptr<path_state_t> next_path(new path_state_t(graph_info.max_v_i_, pdcsc->partitioner()));
-    std::shared_ptr<path_state_t> found_path(new path_state_t(graph_info.max_v_i_, pdcsc->partitioner()));
-
-    using context_spec_t = plato::mepa_ag_context_t<std::vector<std::vector<int>>>;
-    using message_spec_t = plato::mepa_ag_message_t<std::vector<std::vector<int>>>;
 
     plato::vid_t actives = 0;
     for (uint32_t epoch_i = 0; epoch_i < FLAGS_n && actives < FLAGS_num; ++epoch_i) {
-
-    if (epoch_i == 0) {
-        plato::vid_t newers = next_path->foreach<int> (
-            [&](plato::vid_t v_i, std::vector<std::vector<int>>* path) {
-                (*path).push_back(std::vector<int>(1, v_i));
-                return 1;
-            },
-            existed.get()
+      if (epoch_i == 0) {
+        next_path->foreach<int> (
+          [&](plato::vid_t v_i, std::vector<std::vector<int>>* path) {
+            (*path).push_back(std::vector<int>(1, v_i));
+            return 1;
+          },
+          existed.get()
         );
-    } else {
-      actives = plato::aggregate_message<std::vector<std::vector<int>>, int, graph_spec_t> (*pdcsc,
-        [&](const context_spec_t& context, plato::vid_t v_i, const adj_unit_list_spec_t& adjs) {
+      } else {
+        plato::aggregate_message<std::vector<std::vector<int>>, int, graph_spec_t> (*pdcsc,
+          [&](const context_spec_t& context, plato::vid_t v_i, const adj_unit_list_spec_t& adjs) {
             std::vector<std::vector<int>> path_bin;
             for (auto it = adjs.begin_; adjs.end_ != it; ++it) {
-                plato::vid_t src = it->neighbour_;
-                for (auto vpath: (*curr_path)[src]) {
-                    bool is_conflict = ((vpath.size() == 1 && vpath[0] == v_i) ? true : false);
-                    for (int i = 1; i < vpath.size(); i++) {
-                        if (vpath[i] == v_i) {
-                            is_conflict = true;
-                            break;
-                        }
-                    }
-
-                    if (!is_conflict) {
-                        vpath.push_back(v_i);
-                        path_bin.push_back(vpath);
-                    }
+              plato::vid_t src = it->neighbour_;
+              for (auto vpath: (*curr_path)[src]) {
+                bool is_conflict = (vpath[0] == v_i && (vpath.size()+1 < std::max(FLAGS_k,(uint32_t)4)) ? true : false);
+                for (int i = 1; i < vpath.size(); i++) {
+                  if (vpath[i] == v_i) {
+                    is_conflict = true;
+                    break;
+                  }
                 }
+
+                if (!is_conflict) {
+                  vpath.push_back(v_i);
+                  path_bin.push_back(vpath);
+                }
+              }
             }
             context.send(message_spec_t { v_i, path_bin });
-        },
-        [&](int /*p_i*/, message_spec_t& msg) {
-          for (auto path : msg.message_) {
-            if (path.size() >= std::max(FLAGS_k,(uint32_t)4) && path[0] == path[path.size()-1]) {
-              (*found_path)[msg.v_i_].push_back(path);
-            } else {
-              (*next_path)[msg.v_i_].push_back(path);
+          },
+          [&](int /*p_i*/, message_spec_t& msg) {
+            for (auto path : msg.message_) {
+              if (path[0] == path[path.size()-1]) {
+                mtx_found[msg.v_i_].lock();
+                (*found_path)[msg.v_i_].push_back(path);
+                mtx_found[msg.v_i_].unlock();
+              } else {
+                mtx_next[msg.v_i_].lock();
+                (*next_path)[msg.v_i_].push_back(path);
+                mtx_next[msg.v_i_].unlock();
+              }
             }
+            return 1;
           }
-          return 1;
+          ,opts
+        );
+      }
+
+      actives = found_path->foreach<int> (
+        [&](plato::vid_t v_i, std::vector<std::vector<int>>* path) {
+          return (*path).size();
         }
-        ,opts
+      );
+
+      std::swap(curr_path, next_path);
+      
+      next_path->foreach<int> (
+        [&](plato::vid_t v_i, std::vector<std::vector<int>>* path) {
+          path->clear();
+          return 0;
+        }
       );
     }
 
-    actives = found_path->foreach<int> (
-      [&](plato::vid_t v_i, std::vector<std::vector<int>>* path) {
-        return (*path).size();
-      }
-    );
-
-    std::swap(curr_path, next_path);
-    
-    next_path->foreach<int> (
-      [&](plato::vid_t v_i, std::vector<std::vector<int>>* path) {
-        path->clear();
-        return 0;
-      }
-    );
-  } 
-
-  {  // save result
-    plato::thread_local_fs_output os(FLAGS_output, (boost::format("%04d_") % cluster_info.partition_id_).str(), true);
-    found_path->foreach<int> (
-      [&](plato::vid_t v_i, std::vector<std::vector<int>>* path) {
-        if ((*path).size() == 0) return 0;
-        auto& fs_output = os.local();
-        fs_output << "node : " << v_i << ' ' << (*path).size() << "\n";
-        return 0;
-      }
-    );
+    {
+      found_path->foreach<int> (
+        [&](plato::vid_t v_i, std::vector<std::vector<int>>* path) {
+          if ((*path).size() == 0) return 0;
+          
+          printf("Node: %d - %d\n", v_i, (*path).size());
+          return 0;
+        }
+      );
+    }
   }
-
   return 0;
 }
 
